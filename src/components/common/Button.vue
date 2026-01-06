8c96c26b0c9ead358bfc879abc15fa08fde2b7f6
Rubert <rubert@4thewords.com>
2026-01-05 23:20:05 -0500

    feat: add Button component and implement storybook buttons layout

---
+++ src/components/common/Button.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { computed } from "vue"
+
+interface ButtonProps {
+    type?: "button" | "submit" | "reset"
+    disabled?: boolean
+    typeStyle?: "primary" | "secondary" | "primary-outlined" | "secondary-outlined"
+    size?: "sm" | "md" | "lg"
+    href?: string
+    target?: "_blank" | "_self" | "_parent" | "_top"
+}
+const props = withDefaults(defineProps<ButtonProps>(), {
+    type: "button",
+    disabled: false,
+    typeStyle: "primary",
+    size: "md",
+    href: undefined,
+    target: "_self",
+})
+
+const emit = defineEmits<{
+    (e: "click", event: MouseEvent): void
+}>()
+
+const classes = computed(() => {
+    const baseClasses = "font-medium rounded-lg cursor-pointer transition-all duration-300"
+
+    const sizeClasses = {
+        sm: "px-2.5 py-1 text-sm",
+        md: "px-3 py-1.5 text-base",
+        lg: "px-3.5 py-2 text-lg",
+    }
+
+    const typeStyleClasses = {
+        primary: "bg-primary-600 text-white hover:bg-primary-700 hover:shadow-[0_0_12px_var(--color-primary-600)] focus:ring-primary-500",
+        secondary: "bg-secondary-500 text-white hover:bg-secondary-600 hover:shadow-[0_0_12px_var(--color-secondary-500)] focus:ring-secondary-400",
+        "primary-outlined": "border border-primary-600 text-primary-600 hover:bg-primary-50 focus:ring-primary-500",
+        "secondary-outlined": "border border-secondary-600 text-secondary-600 hover:bg-secondary-50 focus:ring-secondary-500",
+    }
+
+    return [
+        baseClasses,
+        sizeClasses[props.size || "md"],
+        typeStyleClasses[props.typeStyle || "primary"],
+        props.disabled ? "opacity-50 !cursor-not-allowed" : "",
+    ].join(" ")
+})
+
+const handleClick = (event: MouseEvent) => {
+    if (!props.disabled) {
+        emit("click", event)
+    }
+}
+</script>
+
+<template>
+    <button v-if="!props.href" :class="classes" :type="props.type" :disabled="props.disabled" @click="handleClick">
+        <slot></slot>
+    </button>
+    <a v-else :href="props.disabled ? undefined : props.href" :aria-disabled="props.disabled" :class="classes" @click="handleClick" :target="props.target">
+        <slot></slot>
+    </a>
+</template>
